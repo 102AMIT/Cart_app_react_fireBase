0a036be090267c7b6b460ca0240b28b5795e9061
Amit Kumar Thakur <kingston.amit9830@gmail.com>
2022-12-10 05:27:11 +0530

    adding firebase in our project

--- src/App.js
+++ src/App.js
@@ -1,52 +1,43 @@
-
 import React from "react";
 import Cart from "./Components/Cart";
 import Navbar from "./Components/Navbar";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
+// import firebase  from './index'
 class App extends React.Component {
 
   constructor(){
     super();
     // state is plane simple java Script object 
     this.state={
-       products:[
-        {
-          price :999,
-          title:'Phone',
-          qty :100,
-          img:'https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGhvbmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-          id:1
-        },
-        {
-          price :99,
-          title:'Watch',
-          qty :10,
-          img:'https://images.unsplash.com/photo-1524592094714-0f0654e20314?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8d2F0Y2h8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-          id:2
-        },
-        {
-          price :9999,
-          title:'Laptop',
-          qty :15,
-          img:'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-          id:3
-        },
-        {
-          price :10000,
-          title:'TV',
-          qty :20,
-          img:'https://images.unsplash.com/photo-1593305841991-05c297ba4575?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8dHZ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-          id:4
-        },
-        {
-          price :100,
-          title:'Desktop',
-          qty :18,
-          img:'https://images.unsplash.com/photo-1614624532983-4ce03382d63d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1331&q=80',
-          id:5
-        }
-       ]
+       products:[],
+       loading:true
     }
     // this.increaseQuantity=this.increaseQuantity.bind(this);
+}
+
+componentDidMount(){
+      firebase
+      .firestore()
+      .collection('products')
+      .get()
+      .then((snapshot)=>{
+        console.log(snapshot);
+        snapshot.docs.map((doc)=>{
+          console.log(doc.data());
+        })
+        const products=snapshot.docs.map((doc)=>{
+          const data=doc.data();
+          data['id']=doc.id;
+          return data;
+        })
+        this.setState({
+          products,
+          loading:false
+        })
+      })
+   
 }
 
 handleIncreaseQuantity=(product)=>{
@@ -98,13 +89,16 @@ getCartTotal=()=>{
 
   let cartTotal=0;
   products.map((product)=>{
+    if(product.qty>0){
     cartTotal+=product.qty*product.price
-  })
+    }
+    return '';
+  });
   return cartTotal;
 }
 
   render(){
-  const {products}=this.state;
+  const {products,loading}=this.state;
   return (
     <div className="cart">
       <Navbar count={this.getCartCount()}/>
@@ -114,6 +108,7 @@ getCartTotal=()=>{
         onDecreaseQuantity={this.handleDecreaseQuantity}
         onDeleteProduct={this.handleDeleteProduct}
       />
+      {loading && <h1>Loading Products ....</h1>}
       <div style={{padding:10,fontSize:20}}>TOTAL: {this.getCartTotal()}</div>
 
     </div>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+//  https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDIFhjWCAfXXTh3ge18KfVW3yLuM9tZ2Mo",
+  authDomain: "cart-f6188.firebaseapp.com",
+  projectId: "cart-f6188",
+  storageBucket: "cart-f6188.appspot.com",
+  messagingSenderId: "565980061311",
+  appId: "1:565980061311:web:c02170c46c0bcd3c04294a"
+};
+
+// Initialize Firebase
+const app=firebase.initializeApp(firebaseConfig);
+const db=app.firestore()
+const auth = app.auth();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +30,8 @@ root.render(
     <App />
     </div>
 );
+export {auth};
+export default{db} ;
 
 
 
